d4a0753c69ced667192bf6e4ac0f791d8b7b361b
RomanBass <49683918+RomanBass@users.noreply.github.com>
2020-11-07 18:44:06 +0300

    на проверку

---
+++ js/form-validation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  const roomsNumberSelect = document.querySelector(`#room_number`);
+  const guestsNumberSelect = document.querySelector(`#capacity`);
+
+  window.addressInput.value = `${Math.round(window.mainPinInitialX + window.mainPinWidth / 2)}, ${Math.round(window.mainPinInitialY + window.mainPinHeight / 2)}`;
+
+  const getConformity = function (roomsNumber, guestsNumber) {
+    if (roomsNumber.value === `100` && guestsNumber.value !== `0`) {
+      guestsNumber.setCustomValidity(`Этот номер не для гостей`);
+    } else if (roomsNumber.value !== `100` && guestsNumber.value === `0`) {
+      guestsNumber.setCustomValidity(`Этому варианту соответствует номер в 100 комнат`);
+    } else if (roomsNumber.value !== `100` && (roomsNumber.value < guestsNumber.value || guestsNumber.value === `0`)) {
+      guestsNumber.setCustomValidity(`Количество гостей не соответствует местам в номере`);
+    } else {
+      guestsNumber.setCustomValidity(``);
+    }
+  };
+
+  getConformity(roomsNumberSelect, guestsNumberSelect);
+  guestsNumberSelect.addEventListener(`change`, function () {
+    getConformity(roomsNumberSelect, guestsNumberSelect);
+  });
+  roomsNumberSelect.addEventListener(`change`, function () {
+    getConformity(roomsNumberSelect, guestsNumberSelect);
+  });
+})();
+
---
+++ js/pin.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+  const OFFERS_NUMBER = 8;
+  const PIN_WIDTH = 50;
+  const PIN_HEIGHT = 70;
+  const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`); // вытаскиваем образец пина из html
+
+  let renderOffer = function (offer) { // функция клонирует образец пина, заполняет свойствами и возвращает его
+    let newPin = pinTemplate.cloneNode(true);
+    newPin.querySelector(`img`).src = offer.author.avatar;
+    newPin.style.left = offer.location.x - PIN_WIDTH / 2 + `px`;
+    newPin.style.top = offer.location.y - PIN_HEIGHT + `px`;
+    return newPin;
+  };
+
+  window.pin = {
+    renderOffers(offers) { // функция создаёт пустой фрагмент, генерит пины и вставляет их в этот фрагмент,
+      const fragment = document.createDocumentFragment(); // затем этот фрагмент передаёт в карту.
+      for (let i = 0; i < offers.length; i++) {
+        fragment.appendChild(renderOffer(offers[i]));
+      }
+      window.map.appendChild(fragment);
+    }
+  };
+
+  window.offers = window.data.getOffers(OFFERS_NUMBER); // массив предложений
+})();
